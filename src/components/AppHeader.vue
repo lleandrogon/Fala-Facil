e8a24394da7326d6e8fc6d38ecdcaba3257796e3
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-01-27 13:55:25 -0300

    Iniciando os primeiros códigos

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="container-fluid custom-container">
+        <div class="inicio"></div>
+        <nav class="row py-2">
+            <div class="col-6 d-flex align-items-center">
+                <img :src="logo" alt="Logo" class="logo img-fluid ms-3">
+            </div>
+            <div class="col-6 d-flex d-md-none justify-content-end align-items-center">
+                <i class="menu-burger fa-solid fa-bars me-3" @click="showNav()"></i>
+            </div>
+            <div v-if="navResponsivo" class="text-center d-md-none">
+                <ul class="lista-responsiva">
+                    <li class="sem-marcadores"><a href="" class="sem-sublinhado">Cursos</a></li>
+                    <li class="sem-marcadores"><a href="" class="sem-sublinhado">Cursos</a></li>
+                    <li class="sem-marcadores"><a href="" class="sem-sublinhado">Cursos</a></li>
+                    <li class="sem-marcadores"><a href="" class="sem-sublinhado">Cursos</a></li>
+                    <li class="sem-marcadores"><a href="" class="sem-sublinhado">Cursos</a></li>
+                </ul>
+            </div>
+            <div class="col-6 d-none d-md-flex justify-content-end align-items-center mt-4">
+                <ul class="lista d-flex">
+                    <li class="sem-marcadores"><a href="" class="sem-sublinhado">Cursos</a></li>
+                    <li class="sem-marcadores"><a href="" class="sem-sublinhado">Unidades</a></li>
+                    <li class="sem-marcadores"><a href="" class="sem-sublinhado">Cursos</a></li>
+                    <li class="sem-marcadores"><a href="" class="sem-sublinhado">Cursos</a></li>
+                    <li class="sem-marcadores"><a href="" class="sem-sublinhado">Cursos</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppHeader",
+
+    data() {
+        return {
+            logo: "logo.png",
+            navResponsivo: false
+        }
+    },
+
+    methods: {
+        showNav() {
+            this.navResponsivo = !this.navResponsivo;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .custom-container {
+        width: 100vw;
+        padding-left: 0;
+        padding-right: 0;
+        overflow-x: hidden;
+    }
+
+    .inicio {
+        height: 50px;
+        background-color: var(--vermelho);
+        width: 100%;
+    }
+
+    .logo {
+        width: 100px;
+    }
+
+    .menu-burger {
+        font-size: 50px;
+        cursor: pointer;
+        transition: transform 0.3s ease;
+    }
+
+    .menu-burger:hover {
+        color: var(--vermelho);
+        transform: rotate(90deg);
+    }
+    
+    .lista-responsiva li {
+        padding: 20px 0;
+    }
+
+    .lista-responsiva li a {
+        font-size: 1.7em;
+        color: black;
+        text-transform: uppercase;
+        transition: 0.3s;
+        font-weight: 500;
+    }
+
+    .lista-responsiva li a:hover {
+        color: var(--vermelho);
+    }
+
+    .lista li {
+        margin: 0 20px;
+    }
+
+    .lista li a {
+        font-size: 1.7em;
+        color: black;
+        transition: 0.3s;
+        text-transform: uppercase;
+        font-weight: 500;
+    }
+
+    .lista li a:hover {
+        color: var(--vermelho);
+    }
+
+    @media (max-width: 992px) {
+        .lista li a {
+            font-size: 1.4em;
+        }
+    }
+</style>
---
+++ src/components/AppHome.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :style="{ backgroundImage: backgroundAtual }" class="container-fluid home"></div>
+</template>
+
+<script>
+export default {
+    name: "AppHome",
+
+    data() {
+        return {
+        backgrounds: [
+            'url("https://wallpapers.com/images/hd/english-2000-x-1333-wallpaper-di44s6r4zq9mj991.jpg")',
+            'url("https://wallpapers.com/images/hd/spanish-background-9nuot4rql38etn3h.jpg")',
+            'url("https://csslaval.gouv.qc.ca/wp-content/uploads/2023/05/shutterstock_1274255737_crop-1.jpg")',
+            'url("https://st3.depositphotos.com/1022214/15596/i/450/depositphotos_155963228-stock-photo-question-do-you-want-to.jpg")',
+        ],
+
+        backgroundIndex: 0,
+        backgroundAtual: ""
+        }
+    },
+
+    mounted() {
+        this.backgroundAtual = this.backgrounds[this.backgroundIndex];
+        this.trocaBackground();
+    },
+
+    methods: {
+        trocaBackground() {
+            setInterval(() => {
+                this.backgroundIndex = (this.backgroundIndex + 1) % this.backgrounds.length;
+                this.backgroundAtual = this.backgrounds[this.backgroundIndex];
+            }, 2000)
+        },
+    }
+}
+</script>
+
+<style>
+.home {
+  height: 700px;
+  background-size: cover;
+  background-position: center;
+  background-position: center;
+  transition: background-image 1s ease-in-out;
+}
+</style>
